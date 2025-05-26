c2155edc51ac0e94f07bd6cf8bcb8a5da038672f
GageBachik <loser@gagebachik.com>
2025-05-25 22:34:37 -0700

    created a loop feature

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,7 @@
 // src/index.ts
-console.log('Helloz, peter!');
+
+const array = [1, 2, 3, 4, 5];
+for (let i = 0; i < array.length; i++) {
+  const element = array[i];
+  console.log(`Element at index ${i} is ${element}`);
+}
